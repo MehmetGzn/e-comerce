99363e8022e51a1a8394804fb8cec4514dcdc37f
MehmetGezen <mehmetgznarch96@gmail.com>
2022-12-07 00:47:38 +0100

    slider added

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,13 +1,15 @@
 import styled from "styled-components"
 import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
 import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
+import { useState } from "react";
+import { sliderItems } from '../data'
 
 const Container = styled.div`
     width: 100%;
     height: 90vh;
-    display: flex;
-    background: coral;
+    display: flex; 
     position: relative;
+    overflow: hidden ;
 `
 const Arrow = styled.div`
       width: 50px;
@@ -28,16 +30,90 @@ const Arrow = styled.div`
       &:hover{
         opacity: 90%;
       }
+      z-index: 2;
+`
+
+const Wrapper = styled.div`
+    height: 100%;
+    display: flex;
+    transform: translateX(-${props=>props.slideIndex*100}vw);
+    transition: 1s ease;
+`
+
+const Slide = styled.div`
+    width: 100vw;
+    height: 90vh;
+    display: flex;
+    align-items: center;
+    background-color: #${props => props.bg};
+`
+
+const ImageContatiner = styled.div`
+    height: 100%;
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+const Image = styled.img`
+    height: 85%;
+`
+
+const InfoContatiner = styled.div`
+    flex: 1;
+    padding: 50px;
+`
+const Title = styled.h1`
+    font-style: 70px;
+`
+const Description = styled.p`
+    margin: 50px 0px;
+    font-style: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`
+const Button = styled.button`
+    padding: 10px;
+    font-style: 20px;
+    background-color: transparent;
 `
 
 
+
+
+
 const Slider = () => {
+    const [slideIndex, setSlideIndex] = useState(0)
+    const handleClick = (direction) => {
+        if (direction==='left') {
+            setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2)
+        }
+        if (direction==='right') {
+            setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0)
+        }
+    }
+
   return (
     <Container>
-        <Arrow direction='left'>
+        <Arrow direction='left' onClick={()=>handleClick("left")}>
             <ArrowLeftOutlinedIcon/>
         </Arrow>
-        <Arrow direction='right'>
+        <Wrapper slideIndex={slideIndex}>
+            {sliderItems.map((item)=>(
+                <Slide bg={item.bg}>
+                    <ImageContatiner>
+                        <Image src={item.img}/>
+                    </ImageContatiner>
+                    <InfoContatiner>
+                        <Title>{item.title}</Title>
+                        <Description>{item.desc}</Description>
+                        <Button>SHOP NOW</Button>
+                    </InfoContatiner>
+                </Slide>
+            ))}
+            
+        </Wrapper>
+        <Arrow direction='right' onClick={()=>handleClick("right")}>
             <ArrowRightOutlinedIcon/>
         </Arrow>
     </Container>
---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://png2.cleanpng.com/sh/445673f7810161a0a16b636a7dcbe45f/L0KzQYm3UcI3N5poiZH0aYP2gLBuTgdwdZJzRdhqc3jsf7A0kBhweKFuhtk2YnXkhcXCTcViPpM3Sqs6ZkO2crWATsE3Omk1TKs7MUW0RoqAUMY1OWoATqU3cH7q/kisspng-woman-fashion-shopping-beauty-5a6b2291f33bd7.1628049215169706419963.png",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      img: "https://png2.cleanpng.com/sh/908b66510a486693e82fcd4327326707/L0KzQYm3UcI6N6F4iZH0aYP2gLBuTfZie5luh9C2bXBndb20gvlrcZ8yftN8aHnyfn7phfF2fKoyhdHtZXywRbK9hvUyPWM5UaMANkWxRoO7V8E1QWI2TaMCMki0RoKBUsk6PF91htk=/kisspng-fashion-model-bijin-fashion-beauty-model-5a6fe152491565.6247149115172816182994.png",
+      id: 2,
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
