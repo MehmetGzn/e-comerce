131bb2318fe4e34ec7a62c544e546f3545c5e99a
MehmetGezen <mehmetgznarch96@gmail.com>
2022-12-22 14:57:08 +0100

    router feature added

--- src/data.js
+++ src/data.js
@@ -1,13 +1,13 @@
 export const sliderItems = [
     {
       id: 1,
-      img: "https://png2.cleanpng.com/sh/445673f7810161a0a16b636a7dcbe45f/L0KzQYm3UcI3N5poiZH0aYP2gLBuTgdwdZJzRdhqc3jsf7A0kBhweKFuhtk2YnXkhcXCTcViPpM3Sqs6ZkO2crWATsE3Omk1TKs7MUW0RoqAUMY1OWoATqU3cH7q/kisspng-woman-fashion-shopping-beauty-5a6b2291f33bd7.1628049215169706419963.png",
+      img: "https://png2.cleanpng.com/sh/aa9db1ec6a69d290193f465debef405c/L0KzQYm3UcI3N5poiZH0aYP2gLBuTgdwdZJzRdhqc3jsf7A0kBhweKFuhtk2YnXkhcXCTcViPpM3Sqs6ZkO2crWATsE3Omk1TKs7MUW0RoqAUMY1OWoATqU3cH7q/kisspng-woman-fashion-shopping-beauty-5a6b2291f33bd7.1628049215169706419963.png",
       title: "SUMMER SALE",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "fcf1ed",
     },
     {
-      img: "https://png2.cleanpng.com/sh/908b66510a486693e82fcd4327326707/L0KzQYm3UcI6N6F4iZH0aYP2gLBuTfZie5luh9C2bXBndb20gvlrcZ8yftN8aHnyfn7phfF2fKoyhdHtZXywRbK9hvUyPWM5UaMANkWxRoO7V8E1QWI2TaMCMki0RoKBUsk6PF91htk=/kisspng-fashion-model-bijin-fashion-beauty-model-5a6fe152491565.6247149115172816182994.png",
+      img: "https://png2.cleanpng.com/sh/35350733acb2b84151cf910b3d65be32/L0KzQYm3UcI6N6F4iZH0aYP2gLBuTfZie5luh9C2bXBndb20gvlrcZ8yftN8aHnyfn7phfF2fKoyhdHtZXywRbK9hvUyPWM5UaMANkWxRoO7V8E1QWI2TaMCMki0RoKBUsk6PF91htk=/kisspng-fashion-model-bijin-fashion-beauty-model-5a6fe152491565.6247149115172816182994.png",
       id: 2,
       title: "AUTUMN COLLECTION",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
@@ -15,7 +15,7 @@ export const sliderItems = [
     },
     {
       id: 3,
-      img: "https://png2.cleanpng.com/sh/0f4ae7abefeb3a66c71ec4527745cc14/L0KzQYm3UsA6N6JrfZH0aYP2gLBuTfRzbaR4ReV0aYL3Pcj2jfFvNZZ6itH5ZT3kfrW0lPhmNaZzgeZuZD32hLL7hgMuf5DyedC2NXG6dLe9VvI5apNmfKc3MEe2SIGCV8YyPWI9SqI9NUK2RYi5U75xdpg=/kisspng-dress-skirt-woman-europe-and-the-united-states-woman-5a7df66b8bbad5.0738097615182045235723.png",
+      img: "https://png2.cleanpng.com/sh/4c03a6cafd47b7de8fea45de63860155/L0KzQYm3UsA6N6JrfZH0aYP2gLBuTfRzbaR4ReV0aYL3Pcj2jfFvNZZ6itH5ZT3kfrW0lPhmNaZzgeZuZD32hLL7hgMuf5DyedC2NXG6dLe9VvI5apNmfKc3MEe2SIGCV8YyPWI9SqI9NUK2RYi5U75xdpg=/kisspng-dress-skirt-woman-europe-and-the-united-states-woman-5a7df66b8bbad5.0738097615182045235723.png",
       title: "LOUNGEWEAR LOVE",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "fbf0f4",
@@ -43,34 +43,84 @@ export const sliderItems = [
   export const popularProducts = [
     {
       id:1,
-      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+      img:"https://img01.ztat.net/article/spp-media-p1/6aab008ea936446091701820e20380f4/4d6ef3679c6d4931b0e9b6c38b54198c.jpg?imwidth=1800&filter=packshot",
     },
     {
       id:2,
-      img:"https://png2.cleanpng.com/sh/a42655d40eb8e070ac396a4d8938bba3/L0KzQYm3U8A3N5RriZH0aYP2gLBuTgNsaaVqReVxb3Wwg7BsgftmeqQyiNN9dHX1fn7vjCQub510etN1LYT1dbBrTf9nNahtgeZuLYPrf7b6TcViQZY9SNVrMUW6dYK9TskyPWg7Sqk7MUW1QIS6V8A6QWE9UKM3cH7q/kisspng-skate-shoe-sneakers-pattern-hot-global-trend-of-white-shoes-5a9e80cb157e16.9157627215203370990881.png",
+      img:"https://img01.ztat.net/article/spp-media-p1/d707866f7fbd40aab8e10798e2f7168a/971e22eac8e547869f5ccd84c0a93967.jpg?imwidth=1800&filter=packshot",
     },
     {
       id:3,
-      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+      img:"https://img01.ztat.net/article/spp-media-p1/7ef65eeba5ba486f8020813ec24f0462/b322cfc3773a48f7b41793a2d55bc214.jpg?imwidth=1800&filter=packshot",
     },
     {
       id:4,
-      img:"https://png2.cleanpng.com/sh/1c4d8ead2c52bebce1784cc986082cb4/L0KzQYm3VME4N5DmfZH0aYP2gLBuTgRweKRthAI2bXBwPbvsgf50NZVqhtt2LYPveb60hvl1NaFmhuZ8LXrocbB6TcVibGc4e6Q6MnPpdYi7TsczP2M4SaUBMUW1Q4qBWcU0P2I9TKU3cH7q/kisspng-topshop-mom-jeans-denim-slim-fit-pants-jeans-5ad63c212cfe74.7272313615239895371843.png",
+      img:"https://img01.ztat.net/article/spp-media-p1/59a581d22f03453598f5105f6ccccc97/9a02e2dd1c2f477ea92590e3affe2f87.jpg?imwidth=1800&filter=packshot",
     },
     {
       id:5,
-      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+      img:"https://img01.ztat.net/article/spp-media-p1/ffd57de56e644c2c9b7e2a692c1fb5d7/d0d6a2d83ac641209498f3f64988b0b7.jpg?imwidth=1800&filter=packshot",
     },
     {
       id:6,
-      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+      img:"https://img01.ztat.net/article/spp-media-p1/8c3388acc79e471d957e8f1413937c97/e8d4c9ebd4b64661a9634afe9e82b8fc.jpg?imwidth=1800&filter=packshot",
     },
     {
       id:7,
-      img:"https://png2.cleanpng.com/sh/ec0dc640cba3e7540f87ae85439f4e83/L0KzQYm3UcMyN6t1iZH0aYP2gLBuTgQue5luiuY2aHBydLrsTgNtbZZ7fZ9sbHB3eLr1h711NaRtgeR9LUXkR4OCWcM4OWg3fNQBLkazR4aCWMI6OWY2T6YAOUe5R4GCVMkveJ9s/kisspng-t-shirt-hoodie-sleeve-clothing-t-shirt-5a729937172db6.6075982915174597670949.png",
+      img:"https://img01.ztat.net/article/spp-media-p1/540c0619e3364385a1a177afc1fbea17/f71cff98fc7545709e94cb5aa92ec1a8.jpg?imwidth=1800&filter=packshot",
     },
     {
       id:8,
-      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+      img:"https://img01.ztat.net/article/spp-media-p1/ce1d078c2f7236618fdc808c84a9fc82/a9aeb462a0dc482cab616e81a8baac67.jpg?imwidth=1800&filter=packshot",
+    },
+    {
+      id:9,
+      img:"https://img01.ztat.net/article/spp-media-p1/2f46acac12434539bf0e55eff4c9650f/8535cc8b91724956b9088f1454d145bf.jpg?imwidth=1800&filter=packshot",
+    },
+    {
+      id:10,
+      img:"https://img01.ztat.net/article/spp-media-p1/ba9dc1ab27414f54a889dc879cb1c1f6/a857af8520ea426baea25b50f8fcbf6f.jpg?imwidth=1800&filter=packshot",
+    },
+  ]
+  export const ShirtProducts = [
+    {
+      id:1,
+      img:"https://img01.ztat.net/article/spp-media-p1/81c991a2cca6441eb52a2cb9a4a68d00/73859fbfb5a44f788f673c80ae4df25d.jpg?imwidth=1800&filter=packshot",
+    },
+    {
+      id:2,
+      img:"https://img01.ztat.net/article/spp-media-p1/60881d6c1c01427590f26997f9cc9745/10e6c84f8dd445b1a1bad4894691709b.jpg?imwidth=1800&filter=packshot",
+    },
+    {
+      id:3,
+      img:"https://img01.ztat.net/article/spp-media-p1/7ef65eeba5ba486f8020813ec24f0462/b322cfc3773a48f7b41793a2d55bc214.jpg?imwidth=1800&filter=packshot",
+    },
+    {
+      id:4,
+      img:"https://img01.ztat.net/article/spp-media-p1/59a581d22f03453598f5105f6ccccc97/9a02e2dd1c2f477ea92590e3affe2f87.jpg?imwidth=1800&filter=packshot",
+    },
+    {
+      id:5,
+      img:"https://img01.ztat.net/article/spp-media-p1/ffd57de56e644c2c9b7e2a692c1fb5d7/d0d6a2d83ac641209498f3f64988b0b7.jpg?imwidth=1800&filter=packshot",
+    },
+    {
+      id:6,
+      img:"https://img01.ztat.net/article/spp-media-p1/8c3388acc79e471d957e8f1413937c97/e8d4c9ebd4b64661a9634afe9e82b8fc.jpg?imwidth=1800&filter=packshot",
+    },
+    {
+      id:7,
+      img:"https://img01.ztat.net/article/spp-media-p1/540c0619e3364385a1a177afc1fbea17/f71cff98fc7545709e94cb5aa92ec1a8.jpg?imwidth=1800&filter=packshot",
+    },
+    {
+      id:8,
+      img:"https://img01.ztat.net/article/spp-media-p1/ce1d078c2f7236618fdc808c84a9fc82/a9aeb462a0dc482cab616e81a8baac67.jpg?imwidth=1800&filter=packshot",
+    },
+    {
+      id:9,
+      img:"https://img01.ztat.net/article/spp-media-p1/2f46acac12434539bf0e55eff4c9650f/8535cc8b91724956b9088f1454d145bf.jpg?imwidth=1800&filter=packshot",
+    },
+    {
+      id:10,
+      img:"https://img01.ztat.net/article/spp-media-p1/ba9dc1ab27414f54a889dc879cb1c1f6/a857af8520ea426baea25b50f8fcbf6f.jpg?imwidth=1800&filter=packshot",
     },
   ]
